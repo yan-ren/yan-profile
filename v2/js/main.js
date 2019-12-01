4ea82589a9bf612192196aa3cf0d4e10855a3cad
Yan Ren <ryan.ren3@gmail.com>
2019-12-01 00:13:55 -0500

    popup windown for game, and add work section

--- v2/js/main.js
+++ v2/js/main.js
@@ -1,5 +1,5 @@
 ;(function () {
-	
+
 	'use strict';
 
 
@@ -308,3 +308,20 @@
 
 
 }());
+
+function PopupCenter(url, title, w, h) {
+    // Fixes dual-screen position                         Most browsers      Firefox
+    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
+    var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
+
+    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    var systemZoom = width / window.screen.availWidth;
+		var left = (width - w) / 2 / systemZoom + dualScreenLeft
+		var top = (height - h) / 2 / systemZoom + dualScreenTop
+    var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w / systemZoom + ', height=' + h / systemZoom + ', top=' + top + ', left=' + left);
+
+    // Puts focus on the newWindow
+    if (window.focus) newWindow.focus();
+}
